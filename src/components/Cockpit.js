531bc7079649f541ac3b90448ca85a3ecb7e9c13
justmarous <mmbilnicki@gmail.com>
2022-09-07 22:08:51 +0200

    i suck in time interval

--- src/components/Cockpit.js
+++ src/components/Cockpit.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addSecond, selectTime } from "../redux/reducers/cockpitSlice";
 
 function Cockpit() {
-  const date = new Date();
-  console.log(date);
-  return <h1>hey</h1>;
+  const time = useSelector(selectTime);
+  const dispatch = useDispatch();
+  const [time2, setTime] = useState(0);
+
+  // setInterval(() => {
+  //   dispatch(addSecond());
+  // }, [10000]);
+
+  setInterval(() => {
+    setTime(time2 + 1);
+  }, [1000]);
+
+  console.log(time2);
+  return <h1>time yo: adssd</h1>;
 }
 
 export default Cockpit;
--- src/redux/reducers/cockpitSlice.js
+++ src/redux/reducers/cockpitSlice.js
@@ -1,18 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    time: 0
-}
+  time: 0,
+};
 
 export const drawerSlice = createSlice({
-    name: 'cockpit',
-    initialState,
-    reducers:{
-        randomShit: (state,action)=>{
-            return state
-        },
-    }
-})
+  name: "cockpit",
+  initialState,
+  reducers: {
+    addSecond: (state, action) => {
+      return {
+        time: state.time + 1,
+      };
+    },
+  },
+});
 
-export const {randomShit} = drawerSlice.actions
-export default drawerSlice.reducer
+export const selectTime = (state) => state.cockpit.time;
+
+export const { addSecond } = drawerSlice.actions;
+export default drawerSlice.reducer;
